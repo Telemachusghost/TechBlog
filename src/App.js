54b09a49a2b7cea41b5dda220945905383c35876
root <derick.falk@gmail.com>
2018-07-30 13:42:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Logo from './components/logo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Logo />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './logo.css';
+
+
+const logo =  () => {
+	return (
+		<footer className="pv4 ph3 ph5-m ph6-l logo">
+  		 <a className='home' href="/"></a>
+		</footer>
+ 	);
+}
+
+
+
+export default logo;
